37f72a6122da34783d0b4362e99cb0c143981a99
Francisco J. Perez <javi4t@gmail.com>
2020-12-03 21:10:34 -0500

    updated sql and ERD

--- SQL_queries/etl_project.sql
+++ SQL_queries/etl_project.sql
@@ -1,13 +1,12 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
+﻿-- DROP TABLE "georgia_covid"
+-- DROP TABLE "election_covid"
 CREATE TABLE "election_covid" (
     "county" VARCHAR(30)   NOT NULL,
     "trump" INT   NOT NULL,
     "biden" INT   NOT NULL,
     "jorgensen" INT   NOT NULL,
     "total_county_votes" INT   NOT NULL,
+    "confirmed_covid_cases" INT   NOT NULL,
     "confirmed_per100k" FLOAT   NOT NULL,
     "antigen_positive" INT   NOT NULL,
     "confirmed_deaths" INT   NOT NULL,
@@ -18,19 +17,20 @@ CREATE TABLE "election_covid" (
      )
 );
 
-CREATE TABLE "georgia_income" (
-    "2018" INT   NOT NULL,
+CREATE TABLE "georgia_covid" (
     "county" VARCHAR(30)   NOT NULL,
+    "2018_avg_income" INT   NOT NULL,
+    "confirmed_covid_cases" INT   NOT NULL,
     "confirmed_per100k" FLOAT   NOT NULL,
     "antigen_positive" INT   NOT NULL,
     "confirmed_deaths" INT   NOT NULL,
     "probable_deaths" INT   NOT NULL,
     "hospitalizations" INT   NOT NULL,
-    CONSTRAINT "pk_georgia_income" PRIMARY KEY (
+    CONSTRAINT "pk_georgia_covid" PRIMARY KEY (
         "county"
      )
 );
 
-ALTER TABLE "georgia_income" ADD CONSTRAINT "fk_georgia_income_county" FOREIGN KEY("county")
+ALTER TABLE "georgia_covid" ADD CONSTRAINT "fk_georgia_covid_county" FOREIGN KEY("county")
 REFERENCES "election_covid" ("county");
 
